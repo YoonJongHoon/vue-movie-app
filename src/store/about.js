bfe7c582ed82ca448d4df8d5fe1724095ff1d271
YoonJongHoon <skfriday@naver.com>
2022-03-14 17:09:47 +0900

    Create project

---
+++ src/store/about.js
@@ -0,0 +1,11 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'YOON',
+    email: 'skfriday@naver,com',
+    blog: 'https://nacodings.tistory.com/',
+    phone: '+82-10-8762-2521',
+    // image: 'https://heropy.blog/css/images/logo.png'
+    image: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FITKnY%2FbtrvSA3Y0vI%2Fz61VyYvZropuTAru6vwzL1%2Fimg.png'
+  })
+}
